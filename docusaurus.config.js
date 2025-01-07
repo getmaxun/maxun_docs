cb01546431fd5cb8e55b75955f493b3b0464f5c3
amhsirak <karishmashuklaa@gmail.com>
2025-01-08 00:04:36 +0530

    feat: remove docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,15 +105,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Docs',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
