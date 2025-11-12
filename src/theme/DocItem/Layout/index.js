9a14988917326cd3e0b9bbfb7de33d80bb626016
amhsirak <karishmashuklaa@gmail.com>
2025-11-12 17:58:34 +0530

    feat: move copy page button to right

--- src/theme/DocItem/Layout/index.js
+++ src/theme/DocItem/Layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
-import {useWindowSize} from '@docusaurus/theme-common';
-import {useDoc} from '@docusaurus/plugin-content-docs/client';
+import { useWindowSize } from '@docusaurus/theme-common';
+import { useDoc } from '@docusaurus/plugin-content-docs/client';
 import DocItemPaginator from '@theme/DocItem/Paginator';
 import DocVersionBanner from '@theme/DocVersionBanner';
 import DocVersionBadge from '@theme/DocVersionBadge';
@@ -11,45 +11,60 @@ import DocItemTOCDesktop from '@theme/DocItem/TOC/Desktop';
 import DocItemContent from '@theme/DocItem/Content';
 import DocBreadcrumbs from '@theme/DocBreadcrumbs';
 import ContentVisibility from '@theme/ContentVisibility';
-import styles from './styles.module.css';
 import CopyPageDropdown from '../../../components/CopyLLM';
+import styles from './styles.module.css';
 
-/**
- * Decide if the toc should be rendered, on mobile or desktop viewports
- */
 function useDocTOC() {
-  const {frontMatter, toc} = useDoc();
+  const { frontMatter, toc } = useDoc();
   const windowSize = useWindowSize();
   const hidden = frontMatter.hide_table_of_contents;
   const canRender = !hidden && toc.length > 0;
+
   const mobile = canRender ? <DocItemTOCMobile /> : undefined;
   const desktop =
     canRender && (windowSize === 'desktop' || windowSize === 'ssr') ? (
       <DocItemTOCDesktop />
     ) : undefined;
+
   return {
     hidden,
     mobile,
     desktop,
   };
 }
-export default function DocItemLayout({children}) {
+
+export default function DocItemLayout({ children }) {
   const docTOC = useDocTOC();
-  const {metadata} = useDoc();
+  const { metadata } = useDoc();
+
   return (
     <div className="row">
       <div className={clsx('col', !docTOC.hidden && styles.docItemCol)}>
         <ContentVisibility metadata={metadata} />
         <DocVersionBanner />
-         <CopyPageDropdown />
-        <div className={styles.docItemContainer}>
-          <article>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            width: '100%',
+            marginBottom: '12px',
+          }}
+        >
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
             <DocBreadcrumbs />
             <DocVersionBadge />
+          </div>
+
+          <CopyPageDropdown />
+        </div>
+        <div className={styles.docItemContainer}>
+          <article>
             {docTOC.mobile}
             <DocItemContent>{children}</DocItemContent>
             <DocItemFooter />
           </article>
+
           <DocItemPaginator />
         </div>
       </div>
