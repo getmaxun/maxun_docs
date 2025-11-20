a83540ff9d65290539d72fb9e1404890a07a1352
amhsirak <karishmashuklaa@gmail.com>
2025-11-20 19:38:19 +0530

    fix: -rm unused icons

--- src/theme/DocSidebarItem/Link/index.js
+++ src/theme/DocSidebarItem/Link/index.js
@@ -6,7 +6,7 @@ import Link from "@docusaurus/Link";
 import isInternalUrl from "@docusaurus/isInternalUrl";
 import IconExternalLink from "@theme/Icon/ExternalLink";
 import styles from "./styles.module.css";
-import { FaRocket, FaBrain, FaRegGem, FaMicrochip } from "react-icons/fa";
+import { FaRocket } from "react-icons/fa";
 import { HiOutlineBookOpen } from "react-icons/hi2";
 import { TbBrandDocker } from "react-icons/tb";
 import { RiComputerLine } from "react-icons/ri";
