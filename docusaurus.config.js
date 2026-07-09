3a0e8225bb9fb04d99533d80ac1ec40cf052299f
amhsirak <karishmashuklaa@gmail.com>
2026-07-09 22:00:02 +0530

    chore: lint

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -195,21 +195,21 @@ const config = {
       //   copyright: `Copyright © ${new Date().getFullYear()} Maxun`,
       // },
       announcementBar: {
-      id: 'scrape_robots_release',
-      content:
-      '<a href="https://www.maxun.dev/blog/maxun-summaries" target="_blank" rel="noopener noreferrer">Summarize web pages in one click with Maxun. Learn more →</a>',
-      backgroundColor: '#ff00c3',
-      textColor: '#ffffffff',
-    },
+        id: 'scrape_robots_release',
+        content:
+          '<a href="https://www.maxun.dev/blog/maxun-summaries" target="_blank" rel="noopener noreferrer">Summarize web pages in one click with Maxun. Learn more →</a>',
+        backgroundColor: '#ff00c3',
+        textColor: '#ffffffff',
+      },
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
-    algolia: {
-    appId: process.env.ALGOLIA_APP_ID,
-    apiKey: process.env.ALGOLIA_SEARCH_API_KEY,
-    indexName: process.env.ALGOLIA_INDEX_NAME,
-  },
+      algolia: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_SEARCH_API_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+      },
     }),
 };
 
